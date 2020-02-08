cc228a98da97bd89b6d482bd3750e2a25332b4be
Karl Taht <Karl.Taht@gmail.com>
2020-02-08 15:48:48 -0700

    new post.

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -1,4 +1,9 @@
 var store = [{
+        "title": "GTA Bound!",
+        "excerpt":"You heard it folks, Data Driven Motorsports is headed for national competition levels at Global Time Attack, the Ridge on June 13 and 14. For both of us, this will be our first national-level competition event. We’ve both dabbled around a lot with smaller organizations such as TakeoverNW, where we...","categories": ["s2000","shr"],
+        "tags": [],
+        "url": "http://localhost:4000/s2000/shr/gta-bound/",
+        "teaser":null},{
         "title": "The S2000 Modlist",
         "excerpt":"Coming soon in full color.   ","categories": ["s2000"],
         "tags": [],
